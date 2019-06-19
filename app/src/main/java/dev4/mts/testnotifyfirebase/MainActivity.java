1c4c1a7f5d04d051afa2f14611a1dba4fa188d71
tony <tony@kingkey.com.tw>
2019-06-19 21:42:31 +0800

    init & firebase Test

---
+++ app/src/main/java/dev4/mts/testnotifyfirebase/MainActivity.java
@@ -0,0 +1,33 @@
+package dev4.mts.testnotifyfirebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(MainActivity.this);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+
+                        String token = instanceIdResult.getToken();
+
+                        Log.i("MainActivity","token:" + token);
+                    }
+                });
+    }
+}
